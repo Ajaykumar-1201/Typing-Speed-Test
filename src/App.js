f8fc624565ec33e164c09157e4cd92bbe6db0fbc
Ajay Kumar <ajaymahto20319@gmail.com>
2023-10-21 22:13:06 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Components/Header";
 import Menu from "./Components/Menu";
+import Footer from "./Components/Footer";
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Menu/>
+            <Footer/>
         </div>
     )
 }
